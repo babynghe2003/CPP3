20c73f2dd0afacff125e5c7b426905f5354f5e5b
minhphi <himinhpho@gmail.com>
2023-03-08 21:04:53 +0700

    first

---
+++ Buoi1/Hello.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+  // số 
+  // có thể tính toán được
+  //3 + 3 = 6
+  // 4 - 3 = 1
+  // kí tự (chữ)
+  // Ex: 3445
+  // Số thực: 3.14
+  // Số nguyên: 3 4 5
+  // không thể tính toán được
+  // 3 + 3 = 3
+  // a + b = ab
+  // - * / ( không thể )
+  // Ex: "Hello world"
+
+  //int -2^31 -> 2^31 - 1 
+  //long long -2^63 -> 2^63 -  1
+
+  //float 1.2e-38 -> 3.4e+38
+  //double 2.3e-308 -> 1.7e+308
+  //long double 3.4e-4932 -> 1.1e+4932
+
+  // string "Hello world"
+  // char 'a'
+  //
+  // kieu logic (dung, sai)
+  // boolean
+  // bool: true, false
+  //const double PI = 3.14;
+  // <kiểu dữ liệu> <tên biến> = <giá trị>;
+  //int studentCount = 10;
+  //studentCount = 20;
+
+  //cout << studentCount << endl;
+
+  // Input
+  // cin (console input)
+  // cin >> <tên biến>;
+  
+  //cout << "Hello world" << endl;
+  //string a;
+  //cout << "Nhap vao mot chuoi : " << endl;
+  //getline(cin, a);
+
+  //cout << "Gia tri vua nhap la " << a << endl;
+
+  
+  
+  return 0;
+}
